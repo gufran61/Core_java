209f352a898307a7909a51d0e81862258e26e13e
gufran61 <you@example.com>
2021-10-28 17:18:40 +0530

    5- joining a Thread

--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -1,9 +1,16 @@
 public class ThreadDemo {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
-        for (int i = 0; i <= 10; i++) {
+       // for (int i = 0; i <= 10; i++) {
             Thread t = new Thread(new DownLoadFileTask());
             t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("file is ready to scanned");
+        //}
+
     }
 }
