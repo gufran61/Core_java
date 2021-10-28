2641cc82bf4d95200f262cd644d405c9c1d33f09
gufran61 <you@example.com>
2021-10-28 17:12:33 +0530

    4- Pausing a Thread

--- src/DownLoadFileTask.java
+++ src/DownLoadFileTask.java
@@ -2,5 +2,12 @@ public class DownLoadFileTask implements Runnable{
     @Override
     public void run() {
         System.out.println("downloading a file"+Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("download complete"+Thread.currentThread().getName());
     }
+
 }
